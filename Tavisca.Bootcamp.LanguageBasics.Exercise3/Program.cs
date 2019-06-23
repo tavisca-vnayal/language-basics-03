0c543ef4cbaf16f17f826f98b941d6d5268178c6
tavisca-vnayal <vnayal@tavisca.in>
2019-06-23 14:50:18 +0530

    Solution for Language Basics 03 Problem Statement

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -41,7 +42,102 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
+            int totalMeals = protein.Length;
+            int totalConsumers = dietPlans.Length;
+            int[] menuSelectionIndex = new int[totalConsumers];
+
+            int[] caloriesCountOfEachMeal = new int[totalMeals];
+            for(int i=0;i<totalMeals;i++)
+            {
+                caloriesCountOfEachMeal[i] = 5 * (protein[i] + carbs[i]) + 9 * fat[i];
+            }
+            for(int i=0;i<totalConsumers;i++)
+            {
+                string dietPlanOfIndividual = dietPlans[i];
+                if(dietPlanOfIndividual.Length == 0)
+                {
+                    menuSelectionIndex[i] = 0;
+                }
+                else
+                {
+                    List<int> temporaryListOfIndexes = new List<int>();
+                    for(int j=0;j<totalMeals;j++)
+                    {
+                        temporaryListOfIndexes.Add(j);
+                    }
+
+                    for(int j=0;j<dietPlanOfIndividual.Length;j++)
+                    {
+                        switch(dietPlanOfIndividual[j])
+                        {
+                            case 'P':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes,true,protein);
+                                break;
+                            case 'p':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, protein);
+                                break;
+                            case 'C':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, true, carbs);
+                                break;
+                            case 'c':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, carbs);
+                                break;
+                            case 'F':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, true, fat);
+                                break;
+                            case 'f':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, fat);
+                                break;
+                            case 'T':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, true, caloriesCountOfEachMeal);
+                                break;
+                            case 't':
+                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, caloriesCountOfEachMeal);
+                                break;
+                        }
+                    }
+                    menuSelectionIndex[i] = temporaryListOfIndexes[0];
+                }
+            }
+            return menuSelectionIndex;
             throw new NotImplementedException();
+        }
+
+        public static List<int> recommendMealOfIndividual(List<int> tempIndexes, bool isUpperCase, int[] ingredient)
+        {
+            int keyToBeOptimized = ingredient[tempIndexes[0]];
+            if(tempIndexes.Count > 0 )
+            {
+                if(isUpperCase == true)
+                {
+                    for(int i=1;i<tempIndexes.Count;i++)
+                    {
+                        if(keyToBeOptimized < ingredient[tempIndexes[i]])
+                        {
+                            keyToBeOptimized = ingredient[tempIndexes[i]];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < tempIndexes.Count; i++)
+                    {
+                        if (keyToBeOptimized > ingredient[tempIndexes[i]])
+                        {
+                            keyToBeOptimized = ingredient[tempIndexes[i]];
+                        }
+                    }
+                }
+            }
+
+            List<int> newIndexes = new List<int>();
+            for(int i=0;i<tempIndexes.Count;i++)
+            {
+                if(ingredient[tempIndexes[i]] == keyToBeOptimized) {
+                    newIndexes.Add(tempIndexes[i]);
+                }
+            }
+            return newIndexes;
         }
     }
 }
